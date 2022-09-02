2e370fbe917bd05a41141ab369376799dd763bfa
Sami Mäkelä <sami@httpster.io>
2022-09-03 02:42:33 +0300

    lotsa stuff

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 import PhotoSwipeLightbox from "/assets/photoswipe-lightbox.esm.js";
 
 const lightbox = new PhotoSwipeLightbox({
-  gallery: '#my-gallery',
+  gallery: '#photogallery',
   children: 'a',
   pswpModule: () => import("/assets/photoswipe.esm.js"),
 });
